7aaac0105ab5f1322defdf3ea6d943fd041608e0
houxxam <houxxam95@gmail.com>
2024-03-13 00:06:44 +0000

    Get Update Data input Ready

--- Repas/Repas/wwwroot/js/site.js
+++ Repas/Repas/wwwroot/js/site.js
@@ -10,34 +10,21 @@ $(function () {
     const destination = ["malade", "personnel"];
      
     $("#save-btn").on("click", function () {
-       
-        for (var i = 0; i < destination.length; i++) {
-           
-            
-            $('#' + destination[i] + ' tbody tr').each(function (index, tr) {
-                
+        var totalCount = 0;
+        var completedCount = 0;
 
+        // Loop through each destination
+        for (var i = 0; i < destination.length; i++) {
+            $('#' + destination[i] + ' tbody tr').each(function (index, tr) {
                 var dateId = $("#date").find('span').attr('id');
                 var dest = $(this).closest('div').attr('id');
                 var serviceid = $(tr).find('th').attr('serviceid');
                 var servicetext = $(tr).find('th').text();
-                var typerepas = $("#" + destination[i] +" thead tr th");
-           
+                var typerepas = $("#" + destination[i] + " thead tr th");
+
                 $(tr).find('td').each(function (cellIndex, cell) {
                     var inputVal = $(cell).find('input').val();
 
-
-                   
-                    rowCount = $('#' + destination[i] + ' tbody td').length;
-
-                    console.log("Row Count =>" + rowCount);
-                
-                
-                    console.log("SERVICE Name: " + servicetext);
-                    console.log("SERVICE ID: " + serviceid);
-                    console.log("TYPE REPAS: " + typerepas.eq(cellIndex + 1).attr('typerepas'));
-                    console.log("QTE: " + inputVal);
-                    console.log("-----------------------------------------");
                     var repasData = {
                         TotalRepas: parseInt(inputVal),
                         TypeRepasId: parseInt(typerepas.eq(cellIndex + 1).attr('typerepas')),
@@ -46,43 +33,41 @@ $(function () {
                         DateFornitureId: parseInt(dateId)
                     };
                     var apiEndpoint = 'http://localhost:5092/api/RepasServicesApi';
-                    sendData(apiEndpoint, repasData);
-                
+
+                    sendData(apiEndpoint, repasData, checkCompletion);
+
+                    // Increment total count
+                    totalCount++;
                 });
             });
-            if (i === destination.length - 1) {
-                if (count === rowCount) {
-                    
-                    window.location.href = '/';
-                    console.log('refresh');
+        }
 
-                }
+        // Check completion after all API requests are made
+        function checkCompletion() {
+            completedCount++;
+            if (completedCount === totalCount) {
+                window.location.href = '/';
+                console.log('refresh');
             }
         }
-        // Move this line outside of the loop
-        //window.location.href = '/'; // Redirect after processing all rows
     });
 
 
-
-    function sendData(apiEndpoint, repasData) {
-        console.log("apiEndpoint : " + apiEndpoint);
-        $.post({
+    function sendData(apiEndpoint, repasData, callback) {
+        $.ajax({
             url: apiEndpoint,
+            type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify(repasData),
             success: function (data) {
-                // Handle success
-                count++;
-                console.log("Count : "+count);
-                console.log("rowCount : " + rowCount);
                 console.log('Data sent successfully:', data);
-                // Optionally, you can redirect to another page or perform any other action
-                
+                if (callback) {
+                    callback(); // Invoke the callback function if provided
+                }
             },
             error: function (xhr, status, error) {
-                // Handle error
                 console.error('Error:', error);
+                // You may handle errors here if needed
             }
         });
     }
